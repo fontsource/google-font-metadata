0c485b44c0a26c6a3112a083d4af544bdcb9029c
Lotus <DecliningLotus@users.noreply.github.com>
2021-05-24 14:35:16 +0300

    feat: initial cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import APIv1Import from "./data/google-fonts-v1.json";
 import APIv2Import from "./data/google-fonts-v2.json";
 import APIVariableImport from "./data/variable.json";
-import { FontObjectv2 } from "./api-parser-v2";
-import { FontObjectv1 } from "./api-parser-v1";
-import { FontObjectVariable } from "./variable-parser";
+import type { FontObjectv2 } from "./api-parser-v2";
+import type { FontObjectv1 } from "./api-parser-v1";
+import type { FontObjectVariable } from "./variable-parser";
 
 const APIv1: FontObjectv1 = APIv1Import;
 const APIv2: FontObjectv2 = APIv2Import;
