cdc0bedac119563587ffb6f550d5e7b13c079f99
Lotus <declininglotus@gmail.com>
2021-11-03 00:14:09 +0000

    chore: build lib

--- lib/api-parser-v1.d.ts
+++ lib/api-parser-v1.d.ts
@@ -1,26 +1,18 @@
 import type { FontVariants, APIResponse } from "./index";
 export interface FontObjectV1 {
-  [id: string]: {
-    family: string;
-    id: string;
-    subsets: string[];
-    weights: number[];
-    styles: string[];
-    variants: FontVariants;
-    defSubset: string;
-    lastModified: string;
-    version: string;
-    category: string;
-  };
+    [id: string]: {
+        family: string;
+        id: string;
+        subsets: string[];
+        weights: number[];
+        styles: string[];
+        variants: FontVariants;
+        defSubset: string;
+        lastModified: string;
+        version: string;
+        category: string;
+    };
 }
-export declare const fetchCSS: (
-  font: APIResponse,
-  userAgent: string
-) => Promise<string>;
-export declare const fetchAllCSS: (
-  font: APIResponse
-) => Promise<[string, string, string]>;
-export declare const processCSS: (
-  css: [string, string, string],
-  font: APIResponse
-) => FontObjectV1;
+export declare const fetchCSS: (font: APIResponse, userAgent: string) => Promise<string>;
+export declare const fetchAllCSS: (font: APIResponse) => Promise<[string, string, string]>;
+export declare const processCSS: (css: [string, string, string], font: APIResponse) => FontObjectV1;
