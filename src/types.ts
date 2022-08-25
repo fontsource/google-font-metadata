6354c38a3191cc746da840fd84eb1609ee8b5c52
DecliningLotus <declininglotus@gmail.com>
2022-08-26 00:25:29 +0300

    feat: add license scraper

--- src/types.ts
+++ src/types.ts
@@ -77,23 +77,23 @@ type FontObject = FontObjectV1 | FontObjectV2 | FontObjectVariable;
 // Variable axes - have to put here to prevent circular dependency
 // We manually add support for new axes to ensure they aren't breaking since Google likes to introduce new ways to use them
 const SUPPORTED_AXES_UPPER = [
-  "CASL",
-  "CRSV",
-  "FILL",
-  "GRAD",
-  "MONO",
-  "SOFT",
-  "WONK",
-  "XOPQ",
-  "XTRA",
-  "YOPQ",
-  "YTAS",
-  "YTDE",
-  "YTFI",
-  "YTLC",
-  "YTUC",
+  'CASL',
+  'CRSV',
+  'FILL',
+  'GRAD',
+  'MONO',
+  'SOFT',
+  'WONK',
+  'XOPQ',
+  'XTRA',
+  'YOPQ',
+  'YTAS',
+  'YTDE',
+  'YTFI',
+  'YTLC',
+  'YTUC',
 ] as const;
-const SUPPORTED_AXES_LOWER = ["ital", "opsz", "slnt", "wdth", "wght"] as const;
+const SUPPORTED_AXES_LOWER = ['ital', 'opsz', 'slnt', 'wdth', 'wght'] as const;
 const SUPPORTED_AXES = [
   ...SUPPORTED_AXES_LOWER,
   ...SUPPORTED_AXES_UPPER,
@@ -101,7 +101,7 @@ const SUPPORTED_AXES = [
 
 type SupportedAxes = typeof SUPPORTED_AXES[number];
 
-const STANDARD_AXES = ["opsz", "slnt", "wdth", "wght"] as const;
+const STANDARD_AXES = ['opsz', 'slnt', 'wdth', 'wght'] as const;
 type StandardAxes = typeof STANDARD_AXES[number];
 
 const isAxesKey = (axesKey: string): axesKey is SupportedAxes =>
@@ -109,9 +109,29 @@ const isAxesKey = (axesKey: string): axesKey is SupportedAxes =>
 const isStandardAxesKey = (axesKey: string): axesKey is StandardAxes =>
   STANDARD_AXES.includes(axesKey as StandardAxes);
 
+// License
+
+interface Authors {
+  copyright: string
+  website?: string
+  email?: string
+}
+interface Licenses {
+  [id: string]: {
+    id: string;
+    authors: Authors;
+    license: {
+      type: string
+      url: string;
+    }
+    original: string;
+  }
+}
+
 export { isAxesKey, isStandardAxesKey, STANDARD_AXES, SUPPORTED_AXES };
 
 export type {
+  Authors,
   FontObject,
   FontObjectV1,
   FontObjectV2,
@@ -119,6 +139,7 @@ export type {
   FontObjectVariableDirect,
   FontVariants,
   FontVariantsVariable,
+  Licenses,
   StandardAxes,
   SupportedAxes,
 };
