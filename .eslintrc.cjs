a3f3d246bd955c0a387f1e51522375ec2dca2287
Lotus <declininglotus@gmail.com>
2023-03-01 02:04:29 +0000

    test: fix lint errors in ci

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,5 +2,8 @@ require("@ayuhito/eslint-config/patch");
 
 module.exports = {
   extends: ["@ayuhito/eslint-config/profile/node"],
-  parserOptions: { tsconfigRootDir: __dirname },
+	parserOptions: { tsconfigRootDir: __dirname },
+	rules: {
+		"@typescript-eslint/no-throw-literal": "off",
+	}
 };
